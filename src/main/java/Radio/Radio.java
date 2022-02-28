61497ca9387a61a004ebfe63a7495f5fe1f0a0fb
GLEB KARAPETYAN <carapetyangleb@yandex.ru>
2022-03-01 02:10:14 +0300

    cm2

--- src/main/java/Radio/Radio.java
+++ src/main/java/Radio/Radio.java
@@ -1,10 +1,27 @@
 package Radio;
 
 public class Radio {
+    private int id;
+    private String name = "rad300";
     public int currentChannel;
-    int firstChannel = 0;
-    int lastChannel = 9;
+    public int currentVolume;
+    public int firstChannel = 0;
+    private int lastChannel = 9;
+    private int min = 0;
+    private int max = 100;
 
+    public Radio(int id, String name, int firstChannel, int lastChannel, int min, int max) {
+        this.id = id;
+        this.name = name;
+        this.firstChannel = firstChannel;
+        this.lastChannel = lastChannel;
+        this.min = min;
+        this.max = max;
+    }
+
+    public int getId() {
+        return this.id;
+    }
 
     public void setCurrentChannel1(int next) {
         if (next >= firstChannel) {
@@ -32,13 +49,9 @@ public class Radio {
         return currentChannel;
     }
 
-    public int currentVolume;
-    int firstVolume = 1;
-    int lastVolume = 10;
-
     public void setCurrentVolume1(int currentVolume1) {
 
-        if (currentVolume1 >= firstVolume) {
+        if (currentVolume1 >= min) {
             currentVolume1 = currentVolume1 + 1;
         }
         currentVolume = currentVolume1;
@@ -46,7 +59,7 @@ public class Radio {
 
 
     public void setCurrentVolume2(int currentVolume2) {
-        if (currentVolume2 <= lastVolume) {
+        if (currentVolume2 <= max) {
             currentVolume2 = currentVolume2 - 1;
         }
         currentVolume = currentVolume2;
@@ -57,3 +70,7 @@ public class Radio {
     }
 
 }
+
+
+
+
--- src/test/java/Radio/RadioTest.java
+++ src/test/java/Radio/RadioTest.java
@@ -2,13 +2,15 @@ package Radio;
 
 import org.junit.jupiter.api.Test;
 
+import javax.xml.namespace.QName;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
+    Radio rad = new Radio(1,"rad300",0,9,0,100);
 
     @Test
     void setCurrentChannel1() {
-        Radio rad = new Radio();
         rad.setCurrentChannel1(8);
 
         int expected = 9;
@@ -19,7 +21,6 @@ class RadioTest {
 
     @Test
     void setCurrentChannel2() {
-        Radio rad = new Radio();
         rad.setCurrentChannel2(7);
 
         int expected = 6;
@@ -30,10 +31,9 @@ class RadioTest {
 
     @Test
     void setCurrentVolume1() {
-        Radio rad = new Radio();
-        rad.setCurrentVolume1(1);
+        rad.setCurrentVolume1(79);
 
-        int expected = 2;
+        int expected = 80;
         int actual = rad.getCurrentVolume();
 
         assertEquals(expected, actual);
@@ -41,10 +41,9 @@ class RadioTest {
 
     @Test
     void setCurrentVolume2() {
-        Radio rad = new Radio();
-        rad.setCurrentVolume2(10);
+        rad.setCurrentVolume2(80);
 
-        int expected = 9;
+        int expected = 79;
         int actual = rad.getCurrentVolume();
 
         assertEquals(expected, actual);
